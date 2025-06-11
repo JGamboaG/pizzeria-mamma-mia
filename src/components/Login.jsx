2bbd4c180ecd24f5c59a26d987c84d4620c91ad1
JGamboaG <juangamboa.j@gmail.com>
2025-06-11 01:34:03 -0400

    Commit Hito 2.

---
+++ src/components/Login.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Login = () => {
+const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+  const [success, setSuccess] = useState("")
+
+  const validarDatos = (e) =>{
+    e.preventDefault()
+
+    setSuccess("")
+    setError("")
+
+    if(!email.trim() || !password.trim()){
+      setError("Todos los campos son obligatorios")
+      return
+    }
+
+    if(password.length < 6){
+        setError("La contraseña debe tener al menos 6 caracteres")
+        return
+    }
+
+    setSuccess("Login correcto")
+  }
+
+  return (
+    <div class="d-flex justify-content-center align-items-center min-vh-100 px-3">
+      <div class="border p-4 rounded shadow w-100" style={{ maxWidth: "600px" }}>
+        <h2 class="fw-bold text-center mb-4">Login</h2>
+        <form onSubmit={validarDatos}>
+            {(error || success) && <div className={`alert ${error ? "alert-danger" : "alert-success"}`}>{error || success}</div>}
+
+            <div class="mb-3">
+              <label for="email" class="fw-bold">Email</label>
+              <input type="email" class="form-control" placeholder="Ingresa tu correo" onChange={(e) => (
+                setEmail(e.target.value)
+              )}></input>
+            </div>
+
+            <div class="mb-3">
+              <label for="password" class="fw-bold">Contraseña</label>
+              <input type="password" class="form-control" id="password" placeholder="Contraseña" onChange={(e) => (
+                setPassword(e.target.value)
+              )}></input>
+            </div>
+
+            <button type='submit' class="btn btn-primary btn-sm">Enviar</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/components/Register.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Register = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [error, setError] = useState("")
+  const [success, setSuccess] = useState("")
+
+  const validarDatos = (e) =>{
+    e.preventDefault()
+
+    setSuccess("")
+    setError("")
+
+    if(!email.trim() || !password.trim() || !confirmPassword.trim()){
+      setError("Todos los campos son obligatorios")
+      return
+    }
+
+    if(password.length < 6){
+        setError("La contraseña debe tener al menos 6 caracteres")
+        return
+    }
+
+    if(password !== confirmPassword){
+      setError("Las contraseñas no coinciden")
+      return
+    }
+    setSuccess("Formulario enviado")
+  }
+
+  return (
+    <div class="d-flex justify-content-center align-items-center min-vh-100 px-3">
+      <div class="border p-4 rounded shadow w-100" style={{ maxWidth: "600px" }}>
+        <h2 class="fw-bold text-center mb-4">Registro</h2>
+        <form onSubmit={validarDatos}>
+            {(error || success) && <div className={`alert ${error ? "alert-danger" : "alert-success"}`}>{error || success}</div>}
+
+            <div class="mb-3">
+              <label for="email" class="fw-bold">Email</label>
+              <input type="email" class="form-control" placeholder="Ingresa tu correo" onChange={(e) => (
+                setEmail(e.target.value)
+              )}></input>
+            </div>
+
+            <div class="mb-3">
+              <label for="password" class="fw-bold">Contraseña</label>
+              <input type="password" class="form-control" id="password" placeholder="Contraseña" onChange={(e) => (
+                setPassword(e.target.value)
+              )}></input>
+            </div>
+
+            <div class="mb-3">
+              <label for="confirm-password" class="fw-bold">Confirmar contraseña</label>
+              <input type="password" class="form-control" id="confirm-password" placeholder="Repite la contraseña" onChange={(e) => (
+                setConfirmPassword(e.target.value)
+              )}></input>
+            </div>
+
+            <button type='submit' class="btn btn-primary btn-sm">Enviar</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Register
